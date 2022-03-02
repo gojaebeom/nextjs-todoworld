f0efe27bd5e9fa252a9d8bd6e6a30c70ee83ebfd
gojaebeom <const.gjb@gmail.com>
2022-03-02 17:16:11 +0900

    스케줄 생성 폼 추가

---
+++ src/containers/SchejuleForm.js
@@ -0,0 +1,75 @@
+/* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { ThemeArea, ThemeButton } from "src/components";
+import { useSchejule, useTheme } from "src/hooks";
+
+import { DateRange } from "react-date-range";
+import ko from "date-fns/locale/ko";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css";
+
+export default function SchejuleForm() {
+  const { store } = useSchejule();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [dates, setDates] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const { getMatchedThemeData } = useTheme();
+  const { themeColor } = getMatchedThemeData();
+
+  return (
+    <form
+      onSubmit={handleSubmit((form) => store(form, dates))}
+      className="min-w-[350px] max-w-[350px]"
+    >
+      <ThemeArea
+        reactHookFormObj={{
+          ...register("content", {
+            required: "업무 내용은 필수입니다.",
+            minLength: {
+              value: 2,
+              message: "두글자도 안쓰다니 거 너무한거 아니오..",
+            },
+          }),
+        }}
+        label="업무내용"
+        name="content"
+        type="text"
+        placeholder={`간단하게 일정을 작성해보세요.\n상세한 일정은 더더욱 좋아요!`}
+        errors={errors}
+      />
+
+      <label className="text-xs text-gray-500">날짜선택</label>
+      <DateRange
+        className="w-full"
+        editableDateInputs={true}
+        onChange={(item) => {
+          console.debug(item.selection);
+          console.debug(item.selection);
+          setDates([item.selection]);
+        }}
+        moveRangeOnFirstSelection={false}
+        ranges={dates}
+        months={1}
+        color={themeColor}
+        rangeColors={themeColor}
+        // direction="horizontal"
+        locale={ko}
+      />
+
+      <br />
+
+      <ThemeButton className="mt-2">생성하기</ThemeButton>
+    </form>
+  );
+}
